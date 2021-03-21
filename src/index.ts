8d0730c4729e87432f4d6a9fee4b21fbf512ad77
Brett Bender <brett@benders.net>
2021-03-21 10:31:00 -0700

    add to context menu

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ joplin.plugins.register({
 	onStart: async function() {
 		await joplin.commands.register({
 			name: 'trash',
-			label: 'Trash',
+			label: 'Move to Trash',
 			iconName: 'fas fa-trash-alt',
 			execute: async () => {
 				// Get selected note IDs, bail if none are currently selected
 				const selectedNoteIds = await joplin.workspace.selectedNoteIds();
 				if (selectedNoteIds.length === 0) return;
 
-				// Get the trash folder and create it if it doesn't already exist
+				// Get or create the trash folder
 				let trashFolder = await joplin.data.get(['search'], { type: 'folder', query: 'Trash' });
 				if (!trashFolder.items.length) {
 					trashFolder = await joplin.data.post(['folders'], null, { title: 'Trash' });
@@ -28,5 +28,6 @@ joplin.plugins.register({
 		});
 
 		await joplin.views.toolbarButtons.create('trashButton', 'trash', ToolbarButtonLocation.NoteToolbar);
+		await joplin.views.menuItems.create('trashContextMenuItem', 'trash', MenuItemLocation.NoteListContextMenu);
 	},
 });
