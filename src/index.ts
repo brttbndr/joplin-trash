9fda0070b8d7fe0c0318f0299d8b080227c7da96
Brett Bender <brett@benders.net>
2021-03-21 08:48:39 -0700

    example code from https://discourse.joplinapp.org/t/trashcan/3998/16
    This doesn't build, but wanted to capture baseline

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,27 @@ import joplin from 'api';
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Test plugin started!');
+		await joplin.commands.register({
+			name: 'trash',
+			label: 'Trash',
+			iconName: 'fas fa-trash-alt',
+			execute: async () => {
+				// Get the selected note and exit if none is currently selected
+				const selectedNote = await joplin.workspace.selectedNote();
+				if (!selectedNote) return;
+				
+				// Get the trash folder and create it if it doesn't already exist
+				let trashFolder = await joplin.data.get('search', { type: 'folder', query: 'Trash' });
+				
+				if (!trashFolder.items.length) {
+					trashFolder = await joplin.data.post('folders', { title: 'Trash' });
+				} else {
+					trashFolder = trashFolder.items[0];
+				}
+
+				// Move the note to the trash
+				await joplin.data.put(['notes', selectedNote.id], { parent_id: trashFolder.id });
+			},
+		});
 	},
 });
